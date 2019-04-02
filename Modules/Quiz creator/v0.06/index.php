442f8c846d24edf65d1ce1789bc636a80fe78bbf
EmileAventus <39726851+EmileAventus@users.noreply.github.com>
2019-04-02 23:10:48 +0200

    Add files via upload

---
+++ Modules/Quiz creator/v0.06/index.php
@@ -0,0 +1,43 @@
+<?php
+  require_once "Classes/QuizCreator.php";
+?>
+
+<html>
+
+  <head>
+    <title> Quiz Creator Demo - v0.01 </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
+  </head>
+
+  <?php
+    $quizCreator = new QuizCreator();
+  ?>
+
+  <body>
+    <main>
+      <?php
+        if ( isset($_POST["create_quiz"]) ) {
+          echo "<h1>Quiz successfully created</h1>";
+          $quizCreator -> collectResults();
+          echo $quizCreator -> displayResults();
+        } else if ( !isset($_POST["create_quiz"]) ) {
+          echo "<h1>Quiz Creator Demo - v0.06</h1>";
+          echo $quizCreator -> drawControlPanel();
+          if (isset($_POST["numb_of_questions"]) ) {
+            echo $quizCreator -> drawQuestionPanel();
+            $numbOfQuestions = $quizCreator -> getNumbOfQuestions();
+            echo "
+              <script>
+                var numbOfQuestions = $numbOfQuestions;
+              </script>
+              <script src='js/quiz_creator_radios.js'></script>
+            ";
+          }
+        } 
+      ?>
+    </main>
+  </body>
+
+</html>
